119a6604233c8db2e526b70e791e6e7f2280d530
mohamed-ali-agourram <mohamedaliagourram@gmail.com>
2025-02-18 16:51:17 +0100

    Two Sum (Easy)

---
+++ java/TwoSum.java
@@ -0,0 +1,24 @@
+
+// https://leetcode.com/problems/two-sum/description/
+import java.util.Arrays;
+
+class TwoSum {
+    public static void main(String[] args) {
+        int[] solution = solution(new int[] { 2, 7, 11, 15 }, 9);
+        System.out.println(Arrays.toString(solution));
+    }
+
+    public static int[] solution(int[] nums, int target) {
+        int[] solution = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    solution[0] = i;
+                    solution[1] = j;
+                    break;
+                }
+            }
+        }
+        return solution;
+    }
+}
